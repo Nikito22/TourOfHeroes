7c3e9960ed8a6392ea7a046bea8885bbf87846d2
Nikito22 <njimenez22@gmail.com>
2016-12-12 03:50:53 +0100

    Cambio de 1 superheroe

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -4,7 +4,7 @@ import { Heroe } from './heroe';
 export const HEROES: Heroe[] = [
     { id: 1, nombre: 'El capitan Trueno' },
     { id: 2, nombre: 'El guerrero del antifaz' },
-    { id: 3, nombre: 'Bombasto' },
+    { id: 3, nombre: 'Spiderman' },
     { id: 4, nombre: 'Celeritas' },
     { id: 5, nombre: 'Magneta' },
     { id: 6, nombre: 'RubberMan' },
